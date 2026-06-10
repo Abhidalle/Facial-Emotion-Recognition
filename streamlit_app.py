acfa210f1622f474b09abb979283b3f4daf96456
Abhidalle <abhiadhikari2009@gmail.com>
2026-06-10 11:31:59 +0545

    Add new streamlit_app.py for deploying in the streamlit and add some features for input and image resizing and color chanellings

---
+++ streamlit_app.py
@@ -0,0 +1,49 @@
+# Import the necessary libraries for this streamlit app
+import streamlit as st
+import cv2
+import numpy as np 
+from keras.models import load_model
+
+#Start with the basic UI components like titles and descriptions
+st.title("AI Emotion Analyzer")
+st.write("Check the box below for turning your webcam on")
+
+#Use the cache_resource 
+@st.cache_resource
+def load_my_brain():
+    return load_model("best_model.h5")
+
+emotion_model = load_my_brain()
+emotion_labelings = ["Anger", "Disgust", "Fear", "Happy","Neutral", "Sad", "Surprise"]
+
+#This is only used to find teh faces and it was that same pre trained model i was taking about from Open CV
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+
+#Add Simple checkbox to open camera
+run_camera = st.checkbox("Turn Camera ON")
+
+#Add empty box that will hold teh video frames on teh web
+video_placeholder = st.image([])
+ 
+if run_camera:
+    cap = cv2.VideoCapture(0)
+    
+    while run_camera:
+        ret, frame = cap.read()
+        if not ret:
+            st.write("Camera does not seem to be working")
+            break
+
+        #Make the images of gray color (SINCE WE TRAINED ON B&W IMAGES) so the model can find it easily 
+        gray = cv2.cvtCOlor(frame, cv2.COLOR_BGR2GRAY)
+
+        #Find the faces by ignoring the backgrounds
+        faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=6, minSize=(120,120))
+
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y), (x+w, y+h), (0,255,0), 2)
+
+            # Crop the face and then finally reize them as well
+            face_crop = frame[y:y+h, x:x+w]
+            face_crop = cv2.resize(face_crop, (224, 224))
+            face_crop = cv2.cvtColor(face_crop, cv2.COLOR_BGR2RGB)
